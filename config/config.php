ddc9455111d19703b270c3c29a6e8064600e5f13
nm <617487243@qq.com>
2020-02-24 10:59:03 +0800

    test2

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@ return [
     'refresh_cache' => 0,
 
     // Api service host.
-    'api_base_url' => 'http://hj.api.zz314.com/huajun', //tes
+    'api_base_url' => 'http://hj.api.zz314.com/huajun', //test001
 
     // Models alias map to class.
     'models' => [
